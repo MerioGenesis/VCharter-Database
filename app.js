360a249d6424015ee078a851fed0fc7257ce1c55
MerioGenesis <mariopif20@hotmail.com>
2025-12-22 13:10:31 +0000

    Refactor users and userTypes routes to separate routers
    Moved users and userTypes route logic from app.js into dedicated routers (users-router.js and userTypes-router.js). Updated app.js to use these routers, improving modularity and maintainability.

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ import express from 'express';
 import cors from "cors";
 import vehiclesRouter from './routers/vehicles-router.js';
 import vehicleTypesRouter from './routers/vehicleTypes-router.js';
+import usersRouter from './routers/users-router.js';
+import userTypesRouter from './routers/userTypes-router.js';
 
 // Configure express app -----------------------
 const app = new express();
@@ -20,63 +22,7 @@ app.use(cors({ origin: '*' }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
-const buildUsersReadQuery = (id, variant) => {
-    let sql = '';
-    const table = '((users LEFT JOIN userTypes on users.u_ut_userTypeId = userTypes.ut_id))';
-    const fields = ['u_id', 'u_f_name', 'u_l_name', 'u_gender', 'u_dob', 'u_address', 'u_city', 'u_postcode', 'u_email', 'u_phone', 'u_ut_userTypeId'];
-
-    switch (variant) {
-        case 'types':
-            sql = `SELECT ${fields} FROM ${table} WHERE users.u_ut_userTypeId=:ID`;
-            break;
-        default:
-            sql = `SELECT ${fields} FROM ${table}`;
-            if (id) sql += ` WHERE u_id=:ID`;
-    }
-    return { sql: sql, data: { ID: id } };
-}
-
-const buildUserTypesReadQuery = (id, variant) => {
-    let sql = '';
-    const table = '(userTypes)';
-    const fields = ['ut_id', 'ut_typeName'];
-
-    switch (variant) {
-        default:
-            sql = `SELECT ${fields} FROM ${table}`;
-            if (id) sql += ` WHERE ut_id=:ID`;
-    }
-    return { sql: sql, data: { ID: id } };
-}
-
-
-
-const getUsersController = async (req, res, variant) => {
-    const id = req.params.id;
-    //Validate request
-
-    // Access data
-    const query = buildUsersReadQuery(id, variant);
-    const { isSuccess, result, message: accessorMessage } = await read(query);
-    if (!isSuccess) return res.status(404).json({ message: accessorMessage });
-    // Responses
-    res.status(200).json(result);
-}
-
-const getUserTypesController = async (req, res, variant) => {
-    const id = req.params.id;
-    //Validate request
-
-    // Access data
-    const query = buildUserTypesReadQuery(id, variant);
-    const { isSuccess, result, message: accessorMessage } = await read(query);
-    if (!isSuccess) return res.status(404).json({ message: accessorMessage });
-    // Responses
-    res.status(200).json(result);
-}
-
-// ----------------------ENDPOINTS ---------------------
+// ----------------------ENDPOINTS ---------------------//
 
 // Vehicles
 app.use('/api/vcharter/vehicles', vehiclesRouter);
@@ -87,13 +33,12 @@ app.use('/api/vcharter/vehicletypes', vehicleTypesRouter);
 // ---------------------------------------------
 
 // Users
-app.get('/api/vcharter/users', (req, res) => getUsersController(req, res, null));
-app.get('/api/vcharter/users/:id', (req, res) => getUsersController(req, res, null));
-app.get('/api/vcharter/users/types/:id', (req, res) => getUsersController(req, res, 'types'));
+app.use('/api/vcharter/users', usersRouter);
+// ---------------------------------------------
 
 // UserTypes
-app.get('/api/vcharter/usertypes', (req, res) => getUserTypesController(req, res, null));
-app.get('/api/vcharter/usertypes/:id', (req, res) => getUserTypesController(req, res, null));
+app.use('/api/vcharter/usertypes', userTypesRouter);
+
 
 // Start server --------------------------------
 const PORT = process.env.PORT || 5000;
--- routers/userTypes-router.js
+++ routers/userTypes-router.js
@@ -8,21 +8,19 @@ const router = Router();
 const buildSetFields = (fields) => fields.reduce((setSql, field, index) =>
     setSql + `${field}=:${field}` + ((index === fields.length - 1) ? '' : ','), 'SET ');
 
-const buildUsersReadQuery = (id, variant) => {
+const buildUserTypesReadQuery = (id, variant) => {
     let sql = '';
-    const table = '((users LEFT JOIN userTypes on users.u_ut_userTypeId = userTypes.ut_id))';
-    const fields = ['u_id', 'u_f_name', 'u_l_name', 'u_gender', 'u_dob', 'u_address', 'u_city', 'u_postcode', 'u_email', 'u_phone', 'u_ut_userTypeId'];
+    const table = '(userTypes)';
+    const fields = ['ut_id', 'ut_typeName'];
 
     switch (variant) {
-        case 'types':
-            sql = `SELECT ${fields} FROM ${table} WHERE users.u_ut_userTypeId=:ID`;
-            break;
         default:
             sql = `SELECT ${fields} FROM ${table}`;
-            if (id) sql += ` WHERE u_id=:ID`;
+            if (id) sql += ` WHERE ut_id=:ID`;
     }
     return { sql: sql, data: { ID: id } };
 }
+
 
 // Data accessors ------------------
 
@@ -40,12 +38,12 @@ const read = async (query) => {
 
 // Controllers ----------------------
 
-const getUsersController = async (req, res, variant) => {
+const getUserTypesController = async (req, res, variant) => {
     const id = req.params.id;
     //Validate request
 
     // Access data
-    const query = buildUsersReadQuery(id, variant);
+    const query = buildUserTypesReadQuery(id, variant);
     const { isSuccess, result, message: accessorMessage } = await read(query);
     if (!isSuccess) return res.status(404).json({ message: accessorMessage });
     // Responses
@@ -54,9 +52,9 @@ const getUsersController = async (req, res, variant) => {
 
 // Endpoints -----------------------
 
-router.get('/', (req, res) => getUsersController(req, res, null));
-router.get('/:id', (req, res) => getUsersController(req, res, null));
-router.get('/:id', (req, res) => getUsersController(req, res, 'types'));
+router.get('/', (req, res) => getUserTypesController(req, res, null));
+router.get('/:id', (req, res) => getUserTypesController(req, res, null));
+
 
 
 export default router;
